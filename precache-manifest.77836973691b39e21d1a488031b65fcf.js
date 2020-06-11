b41a895b35837fa237cb418b0cc8cc3a351b20e1
Luay Mallak <luay.mallak@gmail.com>
2020-06-11 14:16:32 +0200

    Updates

---
+++ precache-manifest.77836973691b39e21d1a488031b65fcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffeb948fcf9f20ecdfb3ff10cbd4334",
+    "url": "/https//luaymallak.github.io/cocktailmaster/index.html"
+  },
+  {
+    "revision": "08ca1dd930588a9a6dc9",
+    "url": "/https//luaymallak.github.io/cocktailmaster/static/css/main.96b06d4e.chunk.css"
+  },
+  {
+    "revision": "bbc140015b7a1dc1c3cb",
+    "url": "/https//luaymallak.github.io/cocktailmaster/static/js/2.4ac477d0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/https//luaymallak.github.io/cocktailmaster/static/js/2.4ac477d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ca1dd930588a9a6dc9",
+    "url": "/https//luaymallak.github.io/cocktailmaster/static/js/main.356ed958.chunk.js"
+  },
+  {
+    "revision": "3a4690d6dccf22a2ccaf",
+    "url": "/https//luaymallak.github.io/cocktailmaster/static/js/runtime-main.6fba7036.js"
+  },
+  {
+    "revision": "1b35ed3a9f6ed3e9f77407087e568abb",
+    "url": "/https//luaymallak.github.io/cocktailmaster/static/media/multicolor-glass-cocktail-drinks-black-background-hd-wallpapers.1b35ed3a.jpg"
+  }
+]);
